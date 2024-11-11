4f7ba3b576ca33ff1d2e2cd499de81edf6139008
pranavkagrawal02 <pranavkumaragrawal02@gmail.com>
2024-11-11 10:11:28 +0530

    Add files via upload

---
+++ AppSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeSharingApp
+{
+    public static class AppSettings
+    {
+        //public static string ApiUrl = "https://bikesharingsystemtiet-bhc6acbabsfmcxhm.eastasia-01.azurewebsites.net/";
+        //public static string ApiUrl = "https://localhost:3000/";
+        public static string ApiUrl = "https://bikesharingapp-d2h6dga8g6cmhkc7.canadacentral-01.azurewebsites.net/";
+
+
+
+    }
+}
